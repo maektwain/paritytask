6d4ce60efb4fe97ac49b17fc55c450c854767c53
Maek Twain <saranshisat@icloud.com>
2018-04-12 08:40:02 +0530

    Last

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,19 @@ use web3::futures::Future;
 use clap::App;
 use web3::types::{Address, U256};
 
+use web3::contract::{Contract, Options};
+
 fn main() {
 
      let yaml = load_yaml!("cli.yml");
 
-     let (_eloop, transport) = web3::transports::Http::new("http://localhost:8545").unwrap();
+     let my_account: Address = "0083CD1DAA0677C99908a9180B9E171F8Ed150B4".parse().unwrap();
+
+     let (_eloop, transport) = web3::transports::Http::new("https://ropsten.infura.io/").unwrap();
      let web3 = web3::Web3::new(transport);
      let matches = App::from_yaml(yaml).get_matches();
-      // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-       // required we could have used an 'if let' to conditionally get the value)
-
-        let address = matches.value_of("address").unwrap();
-        let _format: Address = address.parse().unwrap();
-
-        println!("Address balance is : {} ", _format );
+     let address = matches.value_of("address").unwrap();
+     let _format: Address = address.parse().unwrap();
 
         // 1. Send a transaction
 
@@ -31,13 +30,8 @@ fn main() {
         //2. Balance Query
 
         if matches.value_of("INPUT").unwrap() == "balance"{
-            // This comes as vectors
-
-            // for i in address {
-            //     let balance = web3.eth().balance(i, None).wait().unwrap();
-            //     println!("Address balance is : {:?} ", balance );
-            // }
-
+              let balance = web3.eth().balance(_format, None).wait().unwrap();
+                    println!("Address balance is : {} ", balance);
         }
 
 
@@ -45,27 +39,24 @@ fn main() {
         //3. Query Contract
 
         if matches.value_of("INPUT").unwrap() == "contract"{
-            let accounts = web3.eth().accounts().wait().unwrap();
-            // This comes as vectors
+                println!("Address supplied is : {} ", _format);
 
-            for i in accounts {
-                let balance = web3.eth().balance(i, None).wait().unwrap();
-                println!("Address balance is : {:?} ", balance );
-            }
+                let contract = Contract::from_json(web3.eth(), _format, include_bytes!("abi.json")).unwrap();
+                // let result = contract.query("name", (_format,), None, Options::default(), None);
+                // println!("Smart Contract Address Name: {} ", result);
+
+                let _balanceOf = contract.query("balanceOf", (my_account,), None, Options::default(), None);
+                let balance_of: U256 = _balanceOf.wait().unwrap();
+                println!("Smart Contract Balance : {} ", balance_of);
+
 
         }
-
-       // Vary the output based on how many times the user used the "verbose" flag
-       // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
        match matches.occurrences_of("v") {
            0 => println!("No verbose info"),
            1 => println!("Some verbose info"),
            2 => println!("Tons of verbose info"),
            3 | _ => println!("Don't be crazy"),
        }
-
-       // You can handle information about subcommands by requesting their matches by name
-       // (as below), requesting just the name used, or both at the same time
        if let Some(matches) = matches.subcommand_matches("test") {
            if matches.is_present("debug") {
                println!("Printing debug info...");
